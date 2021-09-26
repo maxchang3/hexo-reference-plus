b3302d3f8f29b2ccd6bfc49b56372d615914d0b6
MaxChang3 <maxcfoo@gmail.com>
2021-09-26 15:05:38 +0800

    refactor(jsdom): remove jsdom
    replace jsdom with likedom

--- src/htmlReplacer.js
+++ src/htmlReplacer.js
@@ -1,14 +1,14 @@
-const { JSDOM } = require('jsdom');
+const {parseHTML} = require('linkedom');
 
 module.exports = (data) => {
-    const dom = new JSDOM(data, { runScripts: "dangerously" })
-    const { document, location } = dom.window;
+    let time_s = Date.now();
+    const { document }  = parseHTML(data);
     document.querySelectorAll(".refplus-num").forEach((ref) => {
-        let refid = ref.firstChild.href.replace(location.href, "");
+        let refid = ref.firstChild.href;
         let refnum = document.querySelector(refid).dataset.num;
         ref.firstChild.innerHTML = `[${refnum}]`;
     });
-    let outerHtml = document.body.outerHTML;
-    outerHtml = outerHtml.slice(6, outerHtml.indexOf('</body>'));
+    let time_e = Date.now();
+    let outerHtml = document.toString();;
     return outerHtml
 }
