a0098d5d0fc9f635f719f5db6c22bc6a41f4894c
MaxChang3 <maxcfoo@gmail.com>
2021-09-26 15:06:47 +0800

    fix(htmlReplacer): remove test codes

--- src/htmlReplacer.js
+++ src/htmlReplacer.js
@@ -1,14 +1,12 @@
 const {parseHTML} = require('linkedom');
 
 module.exports = (data) => {
-    let time_s = Date.now();
     const { document }  = parseHTML(data);
     document.querySelectorAll(".refplus-num").forEach((ref) => {
         let refid = ref.firstChild.href;
         let refnum = document.querySelector(refid).dataset.num;
         ref.firstChild.innerHTML = `[${refnum}]`;
     });
-    let time_e = Date.now();
     let outerHtml = document.toString();;
     return outerHtml
 }
