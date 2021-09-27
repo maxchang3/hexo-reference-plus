e4aed3bee6d991d531d3bd0f3070014c356a6b92
MaxChang3 <maxcfoo@gmail.com>
2021-09-27 17:03:36 +0800

    fix(htmlReplacer): error catch

--- src/htmlReplacer.js
+++ src/htmlReplacer.js
@@ -4,7 +4,8 @@ module.exports = (data) => {
     const { document }  = parseHTML(data);
     document.querySelectorAll(".refplus-num").forEach((ref) => {
         let refid = ref.firstChild.href;
-        let refnum = document.querySelector(refid).dataset.num;
+        let refnum = document.querySelector(refid)?.dataset?.num;
+        if(!(refnum)) console.error(`[hexo-reference-plus] Alias error, unknown alias: ${refid}!`)
         ref.firstChild.innerHTML = `[${refnum}]`;
     });
     let outerHtml = document.toString();;
